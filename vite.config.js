940cf56723f603cbb0a0e3342427200d63c90092
seeding <seeding@ihopeso.cn>
2026-07-31 18:24:03 +0800

    feat: vite fonts 添加 system-ui 本地字体配置

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import { bunny } from "laravel-vite-plugin/fonts";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
             fonts: [
-                bunny('Instrument Sans', {
+                bunny("Instrument Sans", {
                     weights: [400, 500, 600],
                 }),
+                {
+                    family: "system-ui",
+                    optimizedFallbacks: false,
+                },
             ],
         }),
         tailwindcss(),
@@ -21,7 +23,7 @@ export default defineConfig({
     server: {
         cors: true,
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
         },
     },
 });
